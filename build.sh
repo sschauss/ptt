4f2e75c025c923330348a21072668cce7850135d
Simon Schauß <s.schauss@bitzeche.de>
2014-05-17 12:48:20 +0200

    added build file

---
+++ build.sh
@@ -0,0 +1,19 @@
+cd ./ass01-client
+echo "install Node.js dependencies..."
+npm install
+echo
+echo
+echo
+echo "install bower dependencies..."
+bower install
+echo
+echo
+echo
+echo "build webapp..."
+grunt build
+echo
+echo
+echo
+echo "copy webapp to servers assets folder"
+cp -r ./dist/* ../ass01-server/src/main/resources/assets
+
