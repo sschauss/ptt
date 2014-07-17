67f65528339fc98049fa80266da69ffc56942081
Simon Schauß <s.schauss@bitzeche.de>
2014-07-17 10:10:38 +0200

    changed jdk to 1.7 and removed htmlmin from grunt build task

--- ass03-client/Gruntfile.js
+++ ass03-client/Gruntfile.js
@@ -464,8 +464,8 @@ module.exports = function (grunt) {
     'cssmin',
     'uglify',
     'filerev',
-    'usemin',
-    'htmlmin'
+    'usemin'
+    //'htmlmin'
   ]);
 
   grunt.registerTask('default', [
